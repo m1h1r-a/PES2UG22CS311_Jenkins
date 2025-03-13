4ff24371bba14645ce6591bfb12819573f917bb6
m1h1r-a <mihir.annambhotla@gmail.com>
2025-03-13 14:54:26 +0530

    second change/commit to hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
     cout << "Hello, World\n";
     cout << "Hello, Jenkins\n";
     cout << "SRN: PES2UG22CS311";
+    cout << "Name : Mihir A";
     cout << "I have successfully built and run this  \n";
     return 0;
 }
